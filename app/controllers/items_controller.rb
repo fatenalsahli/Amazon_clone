c9b52883b95afadd6ef20b172b0ddb5f7c1093d6
Faten <a0@NINI7-Macbook.local>
2022-02-19 04:59:44 +0300

    Uplode multiple files

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,6 +6,8 @@ class ItemsController < ApplicationController
     @items = Item.all
   end
 
+
+
   # GET /items/1 or /items/1.json
   def show
   end
@@ -67,6 +69,6 @@ class ItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def item_params
-      params.require(:item).permit(:name, :price)
+      params.require(:item).permit(:name, :price , :cover , uploads: [])
     end
 end
--- config/application.rb
+++ config/application.rb
@@ -19,4 +19,7 @@ module AmazonClone
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
+
+  
+
 end
